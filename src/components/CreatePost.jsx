2327d640d194b74d49eabbef0862aa2b79eaac8a
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-07-15 12:23:00 +0530

    resolved Create Post issue

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,61 +1,119 @@
 import { useContext, useRef } from "react";
 import { PostList } from "../store/Post-list-store";
 
+function CreatePost() {
+  const { addPost } = useContext(PostList);
 
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const viewsElement = useRef();
+  const tagsElement = useRef();
 
-function CreatePost () {
-    const {addPost} = useContext(PostList);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const views = viewsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
 
-    const userIdElement =  useRef();
-    const postTitleElement =  useRef();
-    const postBodyElement =  useRef();
-    const viewsElement =  useRef();
-    const tagsElement =  useRef();
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    viewsElement.current.value = "";
+    tagsElement.current.value = "";
+    
+    fetch("https://dummyjson.com/posts/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+        views: views,
+        userId: userId,
+        tags: tags,
+      }),
+    })
+      .then((res) => res.json())
+      .then(resObj => addPost(resObj));
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const userId = userIdElement.current.value;
-        const postTitle = postTitleElement.current.value;
-        const postBody = postBodyElement.current.value;
-        const views = viewsElement.current.value;
-        const tags = tagsElement.current.value.split(" ");
-
-        userIdElement.current.value = "";
-        postTitleElement.current.value = "";
-        postBodyElement.current.value = "";
-        viewsElement.current.value = "";
-        tagsElement.current.value = "";
-
-        addPost(userId, postTitle, postBody, views, tags);
-    };
-
-    return (
-        <form id="post-form" className="m-4 create-post" onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="userId" className="form-label fw-semibold">Enter your user Id</label>
-                <input type="text" ref={userIdElement} className="form-control border border-secondary" id="userId" placeholder="Your user Id" required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="title" className="form-label fw-semibold">Enter the title</label>
-                <input type="text" ref={postTitleElement} className="form-control border border-secondary" id="title" placeholder="How are you feeling today....." required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="body" className="form-label fw-semibold">Enter the content</label>
-                <textarea rows={4} ref={postBodyElement} type="text" className="form-control border border-secondary" id="body" placeholder="Tell us more about it" required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="views" className="form-label fw-semibold">Number of views</label>
-                <input type="number" ref={viewsElement} className="form-control border border-secondary" id="views" placeholder="How many people viewed this" required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="tags" className="form-label fw-semibold">Enter your hashtags</label>
-                <input type="text" ref={tagsElement} className="form-control border border-secondary" id="tags" placeholder="Please enter tags using space" required/>
-            </div>
-            <div>
-                <button type="submit" className="btn btn-primary border-dark" >Post</button>
-            </div>
-        </form>
-    );
-};
+  return (
+    <form id="post-form" className="m-4 create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label fw-semibold">
+          Enter your user Id
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          className="form-control border border-secondary"
+          id="userId"
+          placeholder="Your user Id"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label fw-semibold">
+          Enter the title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control border border-secondary"
+          id="title"
+          placeholder="How are you feeling today....."
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label fw-semibold">
+          Enter the content
+        </label>
+        <textarea
+          rows={4}
+          ref={postBodyElement}
+          type="text"
+          className="form-control border border-secondary"
+          id="body"
+          placeholder="Tell us more about it"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="views" className="form-label fw-semibold">
+          Number of views
+        </label>
+        <input
+          type="text"
+          ref={viewsElement}
+          className="form-control border border-secondary"
+          id="views"
+          placeholder="How many people viewed this"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label fw-semibold">
+          Enter your hashtags
+        </label>
+        <input
+          type="text"
+          ref={tagsElement}
+          className="form-control border border-secondary"
+          id="tags"
+          placeholder="Please enter tags using space"
+          required
+        />
+      </div>
+      <div>
+        <button type="submit" className="btn btn-primary border-dark">
+          Post
+        </button>
+      </div>
+    </form>
+  );
+}
 
 export default CreatePost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,32 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/Post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
-import { useState } from "react";
 import Loading from "./Loading";
 
 function PostList() {
-  const { postList, addInitialPosts } = useContext(PostListData);
-  const [fetching, setFetching] = useState(false);
-
-  useEffect(() => {
-    setFetching(true);
-
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    fetch("https://dummyjson.com/posts", { signal })
-      .then((res) => res.json())
-      .then((data) => {
-        addInitialPosts(data.posts);
-        setFetching(false);
-      });
-
-      return () => {
-        console.log("clearing");
-        controller.abort();
-      }
-  }, []);
+  const { postList, fetching } = useContext(PostListData);
 
   return (
     <>
